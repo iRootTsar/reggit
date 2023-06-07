2ec071bc0ca1794e5c8a4ee4f44e8b32b7084b57
Øystein H. Vikane <hvikane94@gmail.com>
2023-06-07 17:02:01 +0200

    Added .net api, docker-compose file for postgres db

---
+++ api/Controllers/VisitController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace reggit_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VisitController : ControllerBase
+{
+    
+
+    private readonly ILogger<VisitController> _logger;
+
+    public VisitController(ILogger<VisitController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetVisitors")]
+    public IEnumerable<Visitor> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Visitor
+        {
+            Name = "Bjartmar",
+            Email = "vlad@komponent.no"
+
+        })
+        .ToArray();
+    }
+
+
+}
+
+public record Visitor
+{
+    string Name; 
+
+    string Email;
+
+}
